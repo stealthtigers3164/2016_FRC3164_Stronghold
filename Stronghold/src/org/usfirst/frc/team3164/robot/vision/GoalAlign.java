59fb0669aa064b275d5e6f55626583f63f527699
William Yount <way1225@live.com>
2016-02-17 17:11:45 -0500

    added and fixed functions

--- Stronghold/src/org/usfirst/frc/team3164/robot/vision/GoalAlign.java
+++ Stronghold/src/org/usfirst/frc/team3164/robot/vision/GoalAlign.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team3164.robot.vision;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 public class GoalAlign {
 
-	private /*final*/ NetworkTable grip = NetworkTable.getTable("grip");
+	private NetworkTable grip = NetworkTable.getTable("grip");
 	private String reportName;
 	public double area;
 	public double centerX;
@@ -11,15 +11,32 @@ public class GoalAlign {
 	public double height;
 	public double solidity;
 	
+	private double imageHeight = 0.75;
+	private double imageWidth = 0.75;
+	
 	public GoalAlign(String ContourReportName) {
 		this.reportName = ContourReportName;//GoalContours i think
 	}
 	
-	public updateByLargestArea() {
-		int largestAreaYet = 0;
-		for (double blobArea : grip.getNumberArray(reportName + "/area", new double[0])) {
-			if(largestAreaYet < area)largestAreaYet = blobArea;
+	public void updateByLargestArea() {
+		int largestIndex = 0;
+		double[] arrayArea = grip.getNumberArray(reportName + "/area", new double[0]);
+		for (int i = 0; i < arrayArea.length; i++) {
+			if(arrayArea[i] > arrayArea[largestIndex])largestIndex = i;
 		}
+		this.area		= grip.getNumberArray(reportName + "/area", new double[0])[largestIndex];
+		this.centerX	= grip.getNumberArray(reportName + "/centerX", new double[0])[largestIndex];
+		this.centerY	= grip.getNumberArray(reportName + "/centerY", new double[0])[largestIndex];
+		this.width		= grip.getNumberArray(reportName + "/width", new double[0])[largestIndex];
+		this.height		= grip.getNumberArray(reportName + "/height", new double[0])[largestIndex];
+		this.solidity	= grip.getNumberArray(reportName + "/solidity", new double[0])[largestIndex];
 	}
 	
+	public double getHorizontalDistanceFromCenter() {
+		return 0.5 * (imageWidth - centerX);
+	}
+	
+	public double getVerticalDistanceFromCenter() {
+		return 0.5 * (imageHeight - centerY);
+	}
 }
